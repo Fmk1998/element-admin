8818feb8fea36d5e2bca980325417b3b27f1b6d6
Fmk1998 <1092911991@qq.com>
2019-12-19 10:29:27 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios.create({
+    baseURL: 'http://localhost:3001/api'
+})
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
